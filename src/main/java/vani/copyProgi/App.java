fbd8a39280f0e8ba1aefaff60226e4ee9d8cd60d
vighvani <50795996+vighvani@users.noreply.github.com>
2019-05-19 18:54:42 +0200

    Add files via upload

---
+++ src/main/java/vani/copyProgi/App.java
@@ -0,0 +1,68 @@
+package vani.copyProgi;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	/*A program olvassa be a fájl első paraméterét, 
+	 * mint szövegfájl, majd írja vissza a konzolra.
+	 * Ha mindkét paraméter meg van adva, akkor a 
+	 * másodikba írja ki a beolvasott fájl tartalmát.*/
+	
+	public static void konzolraIratVagyMasol(String[] args) {
+		String be_utvonal = args[0];
+		String ki_utvonal = args[1];
+		String sor = null;
+		File be_file = new File(be_utvonal);
+		File ki_file = new File(ki_utvonal);
+		//FileReader be = null;
+		//FileWriter ki = null;
+		
+		try (//be = new FileReader(be_file);
+			 //ki = new FileWriter(ki_file);
+			 BufferedReader beolvas = new BufferedReader(new FileReader(be_file));
+			 BufferedWriter kiir = new BufferedWriter(new FileWriter(ki_file, false));) 
+		{
+				while ((sor = beolvas.readLine()) != null) {
+					if (args.length == 2){
+						kiir.write(sor);
+						kiir.newLine();
+					}else if (args.length == 1){
+						System.out.println(sor);
+					}
+				}
+		} catch (FileNotFoundException e) {
+            System.out.println("Rossz az útvonal!");
+		} catch (IOException e) {
+            e.printStackTrace();
+		} /* finally {
+			if (be != null || ki != null) {
+		        try {
+		            be.close();
+		            ki.close();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		    }
+        } */
+	}
+	
+    public static void main( String[] args )
+    {
+        if (args.length > 0) {
+        	konzolraIratVagyMasol(args);
+        } else {
+        	System.err.println("Nem található/nem megfelelő paraméter!");
+        }
+    }
+}
