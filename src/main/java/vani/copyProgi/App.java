743e408f4af6ace2b49598e5f792874bdd87b7eb
vighvani <vigh.vani@gmail.com>
2019-05-21 13:06:06 +0200

    javitas es bovites

--- src/main/java/vani/copyProgi/App.java
+++ src/main/java/vani/copyProgi/App.java
@@ -13,13 +13,31 @@ import java.io.IOException;
  *
  */
 public class App {
-	
-	/*A program olvassa be a fájl első paraméterét, 
-	 * mint szövegfájl, majd írja vissza a konzolra.
-	 * Ha mindkét paraméter meg van adva, akkor a 
-	 * másodikba írja ki a beolvasott fájl tartalmát.*/
-	
-	public static void konzolraIratVagyMasol(String[] args) {
+
+	/*
+	 * A program olvassa be a fájl első paraméterét, mint szövegfájl, majd írja
+	 * vissza a konzolra. Ha mindkét paraméter meg van adva, akkor a másodikba írja
+	 * ki a beolvasott fájl tartalmát.
+	 */
+
+	public static void konzolraIr(String[] args) {
+		String be_utvonal = args[0];
+		String sor = null;
+		File be_file = new File(be_utvonal);
+
+		try (BufferedReader beolvas = new BufferedReader(new FileReader(be_file));) {
+			while ((sor = beolvas.readLine()) != null) {
+				System.out.println(sor);
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Rossz bemeneti útvonal!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void fajlbaMasol(String[] args) {
 		String be_utvonal = args[0];
 		String ki_utvonal = args[1];
 		String sor = null;
@@ -27,42 +45,34 @@ public class App {
 		File ki_file = new File(ki_utvonal);
 		//FileReader be = null;
 		//FileWriter ki = null;
-		
+
 		try (//be = new FileReader(be_file);
 			 //ki = new FileWriter(ki_file);
-			 BufferedReader beolvas = new BufferedReader(new FileReader(be_file));
-			 BufferedWriter kiir = new BufferedWriter(new FileWriter(ki_file, false));) 
-		{
-				while ((sor = beolvas.readLine()) != null) {
-					if (args.length == 2){
-						kiir.write(sor);
-						kiir.newLine();
-					}else if (args.length == 1){
-						System.out.println(sor);
-					}
+				BufferedReader beolvas = new BufferedReader(new FileReader(be_file));
+				BufferedWriter kiir = new BufferedWriter(new FileWriter(ki_file, false));) {
+			while ((sor = beolvas.readLine()) != null) {
+				if (args.length > 1) {
+					kiir.write(sor);
+					kiir.newLine();
+				} else {
+					System.out.println(sor);
 				}
-		} catch (FileNotFoundException e) {
-            System.out.println("Rossz az útvonal!");
+			}
 		} catch (IOException e) {
-            e.printStackTrace();
-		} /* finally {
-			if (be != null || ki != null) {
-		        try {
-		            be.close();
-		            ki.close();
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-		    }
-        } */
+			e.printStackTrace();
+		} /*
+			 * finally { if (be != null || ki != null) { try { be.close(); ki.close(); }
+			 * catch (IOException e) { e.printStackTrace(); } } }
+			 */
 	}
-	
-    public static void main( String[] args )
-    {
-        if (args.length > 0) {
-        	konzolraIratVagyMasol(args);
-        } else {
-        	System.err.println("Nem található/nem megfelelő paraméter!");
-        }
-    }
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			konzolraIr(args);
+		} else if (args.length == 2) {
+			fajlbaMasol(args);
+		} else {
+			System.err.println("Nem található paraméter!");
+		}
+	}
 }
--- src/test/java/vani/copyProgi/AppTest.java
+++ src/test/java/vani/copyProgi/AppTest.java
@@ -1,5 +1,12 @@
 package vani.copyProgi;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +14,153 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+
+	/**
+	 * @Test @DisplayName("Masolas utan ugyanaz-e a ket file tartalma teszt")
+	 */
+	public void testFajlbaMasol() throws IOException {
+		String be_utvonal = "D:\\work\\teszt.txt";
+		String ki_utvonal = "D:\\work\\teszt1.txt";
+		File be_file = new File(be_utvonal);
+		File ki_file = new File(ki_utvonal);
+
+		String[] args = new String[2];
+		args[0] = be_utvonal;
+		args[1] = ki_utvonal;
+
+		App.main(args);
+
+		try (BufferedReader beolvas = new BufferedReader(new FileReader(be_file));
+				BufferedReader teszt_beolvas = new BufferedReader(new FileReader(ki_file));) {
+
+			Assert.assertEquals(beolvas.read(), teszt_beolvas.read());
+		}
+	}
+
+	/**
+	 * @Test @DisplayName("Null-e a beolvasott file teszt")
+	 */
+	public void testKonzolraIr() throws IOException {
+		String be_utvonal = "D:\\work\\teszt.txt";
+		File be_file = new File(be_utvonal);
+
+		String[] args = new String[1];
+		args[0] = be_utvonal;
+
+		App.main(args);
+
+		try (BufferedReader beolvas = new BufferedReader(new FileReader(be_file));) {
+
+			Assert.assertNotNull(beolvas);
+		}
+	}
+
+	/**
+	 * @Test @DisplayName("0 parameter teszt")
+	 */
+	public void testNullParameter() throws IOException {
+		String[] args = new String[0];
+		// App.main(args);
+		String ntp = "Nem talalhato parameter!";
+
+		if (args.length == 1) {
+			System.out.println("Konzolra kiir");
+		} else if (args.length == 2) {
+			System.out.println("Fajlba masol");
+		} else {
+			System.err.println("Nem talalhato parameter!");
+			String ntp1 = "Nem talalhato parameter!";
+
+			// Assert.assertArrayEquals(ntp, ntp1);
+			Assert.assertEquals(ntp, ntp1);
+		}
+	}
+
+	/**
+	 * @Test @DisplayName("Tobb parameter teszt")
+	 */
+	public void testMoreParameter() throws IOException {
+		String[] args = new String[3];
+		// App.main(args);
+		String ntp = "Nem talalhato parameter!";
+
+		if (args.length == 1) {
+			System.out.println("Konzolra kiir");
+		} else if (args.length == 2) {
+			System.out.println("Fajlba masol");
+		} else {
+			System.err.println("Nem talalhato parameter!");
+			String ntp1 = "Nem talalhato parameter!";
+
+			// Assert.assertArrayEquals(ntp, ntp1);
+			Assert.assertEquals(ntp, ntp1);
+		}
+
+	}
+
+	/**
+	 * @Test @DisplayName("1 paraméter teszt")
+	 */
+	public void testOneParameter() throws IOException {
+		String[] args = new String[1];
+		int egy = 1;
+		// App.main(args);
+
+		if (args.length == 1) {
+			System.out.println("Konzolra kiir");
+			int egy1 = 1;
+
+			Assert.assertSame(egy, egy1);
+
+		} else if (args.length == 2) {
+			System.out.println("Fajlba masol");
+		} else {
+			System.err.println("Nem megfelelo parameter!");
+		}
+	}
+
+	/**
+	 * @Test @DisplayName("2 paraméter teszt")
+	 */
+	public void testTwoParameter() throws IOException {
+		String[] args = new String[2];
+		int ketto = 2;
+		// App.main(args);
+
+		if (args.length == 1) {
+			System.out.println("Konzolra kiir");
+		} else if (args.length == 2) {
+			System.out.println("Fajlba masol");
+			int ketto2 = 4;
+
+			Assert.assertNotSame(ketto, ketto2);
+
+		} else {
+			System.err.println("Nem megfelelo parameter!");
+		}
+	}
+
 }
